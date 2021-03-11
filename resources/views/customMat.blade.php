2cc10971b913fdd71b47ea8166cb34100499447a
GessyAvila <gessyavila91@gmail.com>
2021-03-11 15:49:13 -0600

    custom mat

--- resources/views/customMat.blade.php
+++ resources/views/customMat.blade.php
@@ -13,41 +13,35 @@
         </head>
 
         <style>
-            .parent {
-                max-width: 800px;
-                max-height: 800px;
-                width: 100%;
-                height: 100%;
+            .container {
                 position: relative;
-                display: block;
-            }
+                max-width: 100%;
+                min-height: 257px;
+                min-width: 331px;
+                height: auto;
 
-            Fondo {
-                position: relative;
-                top: 0;
-                left: 0;
-                border: 1px red solid;
-
-                margin-top: 50%;
-                margin-left: 50%;
-            }
-
-            parent.Marco {
-                position: relative;
-                top: 0;
-                left: 0;
-                border: 1px red solid;
-                margin-top: 50%;
-                margin-left: 50%;
 
             }
-            parent.Logo {
-                position: relative;
-                top: 0;
-                left: 0;
+
+            .Fondo {
+                position: absolute;
                 border: 1px red solid;
-                margin-top: 50%;
-                margin-left: 50%;
+                min-height: 257px;
+                min-width: 331px;
+            }
+
+            .Marco {
+                position: absolute;
+                border: 1px red solid;
+                min-height: 257px;
+                min-width: 331px;
+
+            }
+            .Logo {
+                position: absolute;
+                border: 1px red solid;
+                min-height: 257px;
+                min-width: 331px;
 
             }
 
@@ -58,45 +52,43 @@
 
         <div class="row">
 
-            <div id="Select" class="col">
+            <div class="col-sm-6" >
+                <div style=" border: 1px blue solid; min-width: 331px;">
+                    <select id="selectFondo" onchange="document.getElementById('fondo').src = this.value"
+                            name="selectFondo">
+                        <option value="{{asset('assets/img/customMat/AppImg/fondo1.png')}}">fondo1</option>
+                        <option value="{{asset('assets/img/customMat/AppImg/fondo2.png')}}">fondo2</option>
+                    </select>
 
-                <div >
-                    <img class="Fondo-parent" id="fondo" src="{{asset('assets/img/customMat/AppImg/fondo1.png')}}"/>
-                    <img class="Marco" id="marco" src="{{asset('assets/img/customMat/AppImg/marco1.png')}}"/>
-                    <img class="Logo" id="centro" src="{{asset('assets/img/customMat/AppImg/centro1.png')}}"/>
-                </div>
-                <select id="selectFondo" onchange="document.getElementById('fondo').src = this.value"
-                        name="selectFondo">
-                    <option value="{{asset('assets/img/customMat/AppImg/fondo1.png')}}">fondo1</option>
-                    <option value="{{asset('assets/img/customMat/AppImg/fondo2.png')}}">fondo2</option>
-                </select>
+                    <select id="selecMarco" onchange="document.getElementById('marco').src = this.value"
+                            name="selectMarco">
+                        <option value="{{asset('assets/img/customMat/AppImg/marco1.png')}}">marco1</option>
+                        <option value="{{asset('assets/img/customMat/AppImg/marco2.png')}}">marco2</option>
+                    </select>
 
-                <select id="selecMarco" onchange="document.getElementById('marco').src = this.value"
-                        name="selectMarco">
-                    <option value="{{asset('assets/img/customMat/AppImg/marco1.png')}}">marco1</option>
-                    <option value="{{asset('assets/img/customMat/AppImg/marco2.png')}}">marco2</option>
-                </select>
-
-                <select id="selecCentro" onchange="document.getElementById('centro').src = this.value"
+                    <select id="selecCentro" onchange="document.getElementById('centro').src = this.value"
+                            name="selectCentro">
                         name="selectCentro">
-                    name="selectCentro">
-                    <option value="{{asset('assets/img/customMat/AppImg/centro1.png')}}">centro1</option>
-                    <option value="{{asset('assets/img/customMat/AppImg/centro2.png')}}">centro2</option>
-                </select>
+                        <option value="{{asset('assets/img/customMat/AppImg/centro1.png')}}">centro1</option>
+                        <option value="{{asset('assets/img/customMat/AppImg/centro2.png')}}">centro2</option>
+                    </select>
+                </div>
+
+                <div class="container"  style="border: 1px orange solid;">
+                    <img class="Fondo" id="fondo" src="{{asset('assets/img/customMat/AppImg/fondo1.png')}}"  alt=""/>
+                    <img class="Marco" id="marco" src="{{asset('assets/img/customMat/AppImg/marco1.png')}}"  alt=""/>
+                    <img class="Logo" id="centro" src="{{asset('assets/img/customMat/AppImg/centro1.png')}}" alt=""/>
+                </div>
 
             </div>
-            <div id="IMG" class="col">
-
-                <div id="Form" class="col-md-4">
-                    <form>
-                        <label for="fname">First name:</label><br>
-                        <input type="text" id="fname" name="fname" value="John"><br>
-                        <label for="lname">Last name:</label><br>
-                        <input type="text" id="lname" name="lname" value="Doe"><br><br>
-                        <input type="submit" value="Submit">
-                    </form>
-                </div>
-
+            <div class="col" style="border: 1px green solid;min-width: 331px;">
+                <form>
+                    <label for="fname">First name:</label><br>
+                    <input type="text" id="fname" name="fname" value="John"><br>
+                    <label for="lname">Last name:</label><br>
+                    <input type="text" id="lname" name="lname" value="Doe"><br><br>
+                    <input type="submit" value="Submit">
+                </form>
             </div>
 
         </div>
