00741b055daca70abd440d3c36e7022f2c5ace87
Gessy Avila <gessyavila91@gmail.com>
2021-03-24 13:37:45 -0600

    env and config name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Elder Protector'),
+    'name' => env('APP_NAME', 'Elder Protectors'),
 
     /*
     |--------------------------------------------------------------------------
