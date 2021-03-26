12b11ba50841437ab94e6927628e72c52f809b3f
kevkabr <kevkabr@gmail.com>
2021-03-25 21:18:58 -0600

    Vista SUCCESS

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -29,6 +29,24 @@
         </head>
         <body>
 
+        <div class="container">
+        <div class="row gutters-sm px-3 py-3 pt-md-5 pb-md-4 ">
+
+
+
+        <div class="jumbotron alert-success">
+            <h1 class="display-4">Success, purchase has be made!</h1>
+            <p class="lead">Your purchase has been successfully made, you can verify the status of your order from your account, or looking for the purchase number in our search engine.</p>
+            <hr class="my-4">
+            <p>Your purchase number is:<span class="badge badge-pill badge-success">XXX-XXX-XXX</span></p>
+            <p>We mail all information, but you can always check your account.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">My account</a> <a class="btn btn-secondary btn-lg" href="#" role="button">Search</a>
+        </div>
+
+
+
+        </div>
+        </div>
 
         </body>
         <footer class="my-5 pt-5 text-muted text-center text-small">
